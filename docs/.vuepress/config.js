f3cb2219ac5f8e6646e6eef32e95a1a231533f4e
haozi <1215423708@qq.com>
2020-03-02 17:47:04 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '小窗铃 Winbell',
+  description: '一款「程序员」和「服务器」之间的交流小程序',
+
+  base : '/',
+  cache: false,
+  port: '80',
+
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    repo: 'haozi4go',// 默认是 false, 设置为 true 来启用，右上角会出现 github 跳转链接
+    lastUpdated: 'Last Updated', // 每个文档展示最近更新时间
+    // editLinks: true, // 设置是否允许别人编辑文档页面
+    nav: [ // 导航栏
+      {text: '主页', link: '/'},
+      {text: '发送消息', link: '/send/'}, // 根路径指docs目录，guide后面加一个 / 表示guide是一个目录，会自动读取其下的 CORE 2 文件展示到页面
+    ],
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true // 为true则开启主页
+heroImage: /images/winbell.png
+features:
+- title: 易用
+  details: 往http://alert.winbell.online/send发送GET请求，就可以收到微信小程序通知。
+- title: 基于微信小程序
+  details: 只要有微信就可以及时接收通知消息，无需额外安装企业微信或钉钉通过机器人来接收消息。
+- title: 历史消息
+  details: 通过小程序『小窗铃』可以查看历史消息
+footer: MIT Licensed | Copyright © 2019-2020 浩子淘天下@窗纸儿吧
+---
+![An image](/images/winbell-mp-white.png)
